064875cafca282b4e6638e192ae626cf64926dd7
Weisburg <WeisburgC@metro.net>
2025-03-28 16:17:14 -0700

    shut it down for the weekend:

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def process_text():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
